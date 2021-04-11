396da411d3cd4e8e5662bebe5d89131fb1397d42
Monika Jóźwiak <zagorska.monika@gmail.com>
2021-04-11 02:00:31 +0200

    added two exercises

--- modules_exercises.py
+++ modules_exercises.py
@@ -1,11 +1,30 @@
-import keyword
+# EX1
+
+# import keyword
+#
+#
+# def contains_keyword(*args):
+#     for arg in args:
+#         if keyword.iskeyword(arg):
+#             return True
+#     return False
+#
+#
+# print(contains_keyword("d", "g"))
+
+# EX2
+from pyfiglet import figlet_format
+from termcolor import colored
+
+def print_art(msg, color):
+    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+    if color not in valid_colors:
+        color = "magenta"
+    ascii_art = figlet_format(msg)
+    ascii_colored = colored(ascii_art, color)
+    print(ascii_colored)
 
 
-def contains_keyword(*args):
-    for arg in args:
-        if keyword.iskeyword(arg):
-            return True
-    return False
-
-
-print(contains_keyword("d", "g"))
+msg = input("What would you like to print? ")
+color = input("What color? ")
+print_art(msg, color)
