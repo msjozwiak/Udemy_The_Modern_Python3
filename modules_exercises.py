58c988cb813a0de0ee2d4e5b1c859eb8463127a7
Monika Jóźwiak <zagorska.monika@gmail.com>
2021-04-18 23:13:14 +0200

    ex3 added

--- modules_exercises.py
+++ modules_exercises.py
@@ -13,18 +13,46 @@
 # print(contains_keyword("d", "g"))
 
 # EX2
-from pyfiglet import figlet_format
-from termcolor import colored
+# from pyfiglet import figlet_format
+# from termcolor import colored
+#
+# def print_art(msg, color):
+#     valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
+#     if color not in valid_colors:
+#         color = "magenta"
+#     ascii_art = figlet_format(msg)
+#     ascii_colored = colored(ascii_art, color)
+#     print(ascii_colored)
+#
+#
+# msg = input("What would you like to print? ")
+# color = input("What color? ")
+# print_art(msg, color)
 
-def print_art(msg, color):
-    valid_colors = ("red", "green", "yellow", "blue", "magenta", "cyan", "white")
-    if color not in valid_colors:
-        color = "magenta"
-    ascii_art = figlet_format(msg)
-    ascii_colored = colored(ascii_art, color)
-    print(ascii_colored)
+# EX3
+# print(__name__)
+
+print("before import")
+import math
+
+print("before functionA")
 
 
-msg = input("What would you like to print? ")
-color = input("What color? ")
-print_art(msg, color)
+def functionA():
+    print("Function A")
+
+
+print("before functionB")
+
+
+def functionB():
+    print("Function B {}".format(math.sqrt(100)))
+
+
+print("before __name__ guard")
+if __name__ == '__main__':
+    functionA()
+    functionB()
+print("after __name__ guard")
+
+print(math)
