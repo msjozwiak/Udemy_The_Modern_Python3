f8a16adfa994a542852b1ad562f6d9c684f254b3
Monika Jóźwiak <zagorska.monika@gmail.com>
2021-04-27 19:16:42 +0200

    rpg_ex.py added

---
+++ rpg_ex.py
@@ -0,0 +1,22 @@
+class Character:
+    def __init__(self, name, hp, level):
+        self.name = name
+        self.hp = hp
+        self.level = level
+
+
+class NPC(Character):
+    def __init__(self, name, hp, level):
+        super().__init__(name, hp, level)
+
+    def speak(self):
+        return "{0} says: 'I heard monsters running around last night!'".format(self.name)
+
+
+villager = NPC("Bob", 100, 12)
+print(villager.name)
+print(villager.hp)
+print(villager.level)
+print(villager.speak())
+
+
