1e64a661cdfe3b354e9f3297ee87502b98425286
Monika Jóźwiak <zagorska.monika@gmail.com>
2021-06-16 13:25:33 +0200

    final changes in scraping_project added

---
+++ csv_scraper.py
@@ -0,0 +1,39 @@
+from bs4 import BeautifulSoup
+import requests
+from time import sleep
+from csv import DictWriter
+
+BASE_URL = "http://quotes.toscrape.com"
+
+
+def scrape_quotes():
+    url = "/page/1"
+    all_quotes = []
+    while url:
+        response = requests.get(f"{BASE_URL}{url}")
+        soup = BeautifulSoup(response.text, "html.parser")
+        quotes = soup.find_all(class_="quote")
+
+        for quote in quotes:
+            all_quotes.append({
+                'text': quote.find(class_="text").get_text(),
+                'author': quote.find(class_="author").get_text(),
+                'href': quote.find('a')['href']
+            })
+        next_btn = soup.find(class_="next")
+        url = next_btn.find('a')['href'] if next_btn else None
+        sleep(1)
+    return all_quotes
+
+
+def write_quotes(quotes):
+    with open("quotes.csv", "w") as file:
+        headers = ["text", "author", "href"]
+        csv_writer = DictWriter(file, fieldnames=headers)
+        csv_writer.writeheader()
+        for quote in quotes:
+            csv_writer.writerow(quote)
+
+
+quotes = scrape_quotes()
+write_quotes(quotes)
--- scraping_project.py
+++ scraping_project.py
@@ -3,6 +3,7 @@ Create a file called `scraping_project.py` which, when run, grabs data on every 
 from the website http://quotes.toscrape.com
 You can use `bs4` and `requests` to get the data. For each quote you should grab the text of the quote, the name of the
 person who said the quote, and the href of the link to the person's bio. Store all of this information in a list.
+
 Next, display the quote to the user and ask who said it. The player will have four guesses remaining.
 After each incorrect guess, the number of guesses remaining will decrement. If the player gets to zero guesses without
 identifying the author, the player loses and the game ends. If the player correctly identifies the author, the player
@@ -19,25 +20,53 @@ Good luck!
 """
 from bs4 import BeautifulSoup
 import requests
-from time import sleep
+from random import choice
+from csv import DictReader
 
-all_quotes = []
-base_url = "http://quotes.toscrape.com"
-url = "/page/1"
+BASE_URL = "http://quotes.toscrape.com"
 
-while url:
-    response = requests.get(f"{base_url}{url}")
-    soup = BeautifulSoup(response.text, "html.parser")
-    quotes = soup.find_all(class_="quote")
 
-    for quote in quotes:
-        all_quotes.append({
-            'text': quote.find(class_="text").get_text(),
-            'author': quote.find(class_="author").get_text(),
-            'href': quote.find('a')['href']
-        })
-    next_btn = soup.find(class_="next")
-    url = next_btn.find('a')['href'] if next_btn else None
-    # sleep(2)
+def read_quotes(filename):
+    with open(filename, "r") as file:
+        csv_reader = DictReader(file)
+        return list(csv_reader)
 
-print(all_quotes)
+
+def start_game(quotes):
+    quote = choice(quotes)
+    remaining_guesses = 4
+    print("Here's a quote: ")
+    print(quote['text'])
+    guess = ""
+    while guess.lower() != quote["author"].lower() and remaining_guesses > 0:
+        guess = input("Who said this quote? Guesses remaining: {} \n".format(remaining_guesses))
+        if guess.lower() == quote["author"].lower():
+            print("You got it right!")
+            break
+        remaining_guesses -= 1
+        if remaining_guesses == 3:
+            res = requests.get("{0}{1}".format(BASE_URL, quote["href"]))
+            soup = BeautifulSoup(res.text, "html.parser")
+            # print(soup.body)
+            birth_date = soup.find(class_="author-born-date").get_text()
+            born_place = soup.find(class_="author-born-location").get_text()
+            print("Here's a hint: The author was born on {} {}".format(birth_date, born_place))
+        elif remaining_guesses == 2:
+            print("Here's a hint: The author's first name starts with: {}". format(quote["author"][0]))
+        elif remaining_guesses == 1:
+            last_initial = quote["author"].split(" ")[1][0]
+            print("Here's a hint: The author's first name starts with: {}".format(last_initial))
+        else:
+            print("Sorry you ran out of guesses. The answer was {}".format(quote["author"]))
+
+    again = ""
+    while again.lower() not in ("y", 'yes', "n", "no"):
+        again = input("Would you like to play again (y/n)?\n")
+    if again.lower() in ("y", 'yes'):
+        return start_game(quotes)
+    else:
+        print("OK, GOODBYE!")
+
+
+quotes = read_quotes("quotes.csv")
+start_game(quotes)
